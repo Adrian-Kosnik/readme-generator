5508158a4ffd8e28babc1f373b5921b01ff7303d
Ordo Adrian <ordoadrian@Ordos-MacBook-Pro.local>
2023-02-17 11:37:20 +0000

    Added a base list of questions to the question arr. Added logic to writeToFile().

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    "What is the title of your project?", "Description?", "Table of Contents?", "Installation?", "Usage?", "License?", "Contributing?", "Tests?", "Questions?"
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.log(err) : console.log('Success!'))
+};
 
 // function to initialize program
 function init() {
